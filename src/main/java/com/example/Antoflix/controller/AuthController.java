1b3fc551134f78e1cbfb79e678e0c70323bd0fd8
Anto-Antonia <nedeeaanto@gmail.com>
2024-10-03 20:31:55 +0300

    added auth controller

---
+++ src/main/java/com/example/Antoflix/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.Antoflix.controller;
+
+import com.example.Antoflix.dto.request.user.AddUserRequest;
+import com.example.Antoflix.dto.request.user.SignInRequest;
+import com.example.Antoflix.dto.response.user.SignInResponse;
+import com.example.Antoflix.service.security.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager) {
+        this.authService = authService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("api/signIn")
+    public ResponseEntity<SignInResponse> signIn(@RequestBody @Valid SignInRequest signInRequest){
+        SignInResponse signInResponse = authService.signIn(signInRequest);
+        return ResponseEntity.ok().body(signInResponse);
+    }
+
+    @PostMapping("api/register")
+    public ResponseEntity<Void> registerAdmin(@RequestBody @Valid AddUserRequest userRequest){
+        authService.getUserByEmail(userRequest.getEmail());
+        authService.registerUser(userRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}
--- src/main/java/com/example/Antoflix/dto/request/user/SignInRequest.java
+++ src/main/java/com/example/Antoflix/dto/request/user/SignInRequest.java
@@ -10,7 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignInRequest {
 
+    @NotEmpty
     private String email;
 
+    @NotEmpty
     private String password;
 }
